2225bb348caf306fc5f0e8b9ba9b0f8c388b336f
kripsenqq <nikitosinka989@gmail.com>
2023-06-29 20:42:11 +0300

    fix

--- calculator.js
+++ calculator.js
@@ -1,3 +1,5 @@
+import {sum, subtract, multiply, divide} from "./variable.js";
+
 function calculator(inputString) {
   const [a, operator, b] = inputString.split(' ');
   let num1 = a;
@@ -16,58 +18,37 @@ function calculator(inputString) {
     X: 10
   }
 
-
-  if (!(num1 in objectAA) || !(num2 in objectAA)) {
-    return "Invalid input";
-  }
-
-  num1 = objectAA[num1];
-  num2 = objectAA[num2];
-
-  if (isNaN(num1) || isNaN(num2)) {
+  if (Object.keys(objectAA).includes(num1) && Object.keys(objectAA).includes(num2)) {
+    num1 = objectAA[num1];
+    num2 = objectAA[num2];
+  } else {
+    if (isNaN(num1) || isNaN(num2)) {
       return "Invalid input";
+    }
   }
 
   let result = null;
 
   switch (operator) {
-      case "+":
-          result = sum(num1, num2);
-          break;
-      case "-":
-          result = subtract(num1, num2);
-          break;         
-      case "*":
-          result = multiply(num1, num2);
-          break;
-      case "/":
-          result = divide(num1, num2);
-          Math.floor(num1/num2);
-          break;
-      default:
-          return "Invalid operator";
-  }
+    case "+":
+        result = sum(num1, num2);
+        break;
+    case "-":
+        result = subtract(num1, num2);
+        break;         
+    case "*":
+        result = multiply(num1, num2);
+        break;
+    case "/":
+        result = divide(num1, num2);
+        Math.floor(num1/num2);
+        break;
+    default:
+        return "Invalid operator";
+    }
 
   result = String(result);
   return result;
-
 }
 
-function sum(a, b) {
-  return a + b;
-}
-
-function subtract(a, b) {
-  return a - b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-function divide(a, b) {
-  return a / b;
-}
-
-
-console.log(calculator("X / V"))
+console.log(calculator("8 / 2"))
